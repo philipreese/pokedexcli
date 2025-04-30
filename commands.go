d52d9bd738d6922175f6d5c2434d0dd804f99413
Philip Reese <philip.reese@gtri.gatech.edu>
2025-04-30 11:08:36 -0400

    Add help and exit commands to repl

---
+++ commands.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, command := range(commandMap) {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -7,13 +7,38 @@ import (
 	"strings"
 )
 
+var commandMap = map[string]cliCommand {}
+
 func main() {
+	commandMap = map[string]cliCommand {
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", input[0])
+
+		command, ok := commandMap[input[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		if err := command.callback(); err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
