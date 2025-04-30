d1237c9d3b033398dc1d410ea3f667c2c66559af
Philip Reese <philip.reese@gtri.gatech.edu>
2025-04-30 08:28:06 -0400

    added initial test case for cleanInput function

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+}
+
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+
+	for i, word := range words {
+		words[i] = strings.ToLower(word)
+	}
+	return words
 }
---
+++ repl_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HELLO  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "   ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Length of actual: %d does not match length of expected: %d", len(actual), len(c.expected))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Actual '%s' does not match expected '%s'", word, expectedWord)
+			}
+		}
+	}
+}
